2acaedeccef8cba778c641aec94adfbe226ae775
Aaditya-Baduni <aadityalaformidable@gmail.com>
2020-11-30 18:21:54 +0530

    more tweaks

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ function noUserName() {
 }
 
 function userNameKnown() {
+    // add user name to cookie
+    userName_cookie = document.getElementById("enter-name").value
     document.getElementById("not-known").style.display = "none"; // make sure that the unknown div is hidden
     document.getElementById("known").style.display = "block";
     // edit known div to show username
