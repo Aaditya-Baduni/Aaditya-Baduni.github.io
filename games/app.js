bd8f1d33a11044ce93e608e1155bfc822d8b7716
Aaditya-Baduni <aadityalaformidable@gmail.com>
2020-12-20 14:20:49 +0530

    edited this to make games webpage work on mobile and tablet

--- games/app.js
+++ games/app.js
@@ -5,25 +5,22 @@ var squirrel_div = document.getElementById("squirrel");
 var rpslisp_div = document.getElementById("rps-li-sp");
 var MyOS_div = document.getElementById("MyOS");
 
-// onclick of those games, open them in a new tab
-rpsGame_div.onclick = function() {
-    window.open("rock-paper-scissors/");
-}
-sounds_div.onclick = function() {
-    window.open("teach-sounds/");
-}
-squirrel_div.onclick = function() {
-    window.open("find-the-squirrel/");
-}
-rpslisp_div.onclick = function() {
-    window.open("rps-lizard-spock/");
-}
-MyOS_div.onclick = function() {
-    window.open("myOS/")
+function openInNewTab(elementLink) {
+    window.location.href = elementLink;
 }
 
-// all a tags to open in a new tab
-var a_tags = document.getElementsByTagName("a");
-for(var i = 0; i < a_tags.length; i++) {
-    a_tags[i].target = "_blank";
+rpsGame_div.onclick = function() {
+    openInNewTab("rock-paper-scissors/");
+}
+sounds_div.onclick = function() {
+    openInNewTab("teach-sounds/");
+}
+squirrel_div.onclick = function() {
+    openInNewTab("find-the-squirrel/");
+}
+rpslisp_div.onclick = function() {
+    openInNewTab("rps-lizard-spock/");
+}
+MyOS_div.onclick = function() {
+    window.open("myOS/");
 }
