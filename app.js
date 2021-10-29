80983171fdd4d8f4bac63237350630dfd341b03c
Aaditya <aadityalaformidable@gmail.com>
2021-10-29 23:42:42 +0530

    added a welcome screen

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ var enterName_input = document.getElementById("enter-name");
 var hello_p = document.getElementById("hello");
 var time_p = document.getElementById("time");
 
-if(typeof(Storage) !== undefined) {
-    if(localStorage.getItem("username") == null) {
-        submitBtn_input.onclick = function() {
+if (typeof (Storage) !== undefined) {
+    // for the user's name
+    if (localStorage.getItem("username") == null) {
+        submitBtn_input.onclick = function () {
             // hide not known div, set username in localStorage
             notKnown_div.style.display = "none";
             localStorage.setItem("username", enterName_input.value);
@@ -21,14 +22,28 @@ if(typeof(Storage) !== undefined) {
         hello_p.innerText = "Hello, " + localStorage.getItem("username");
         known_div.style.display = "block";
     }
+
+    // for the welcome screen
+    if (localStorage.getItem("visited") == "false" || localStorage.getItem("visited") == null) {
+        localStorage.setItem("visited", "true");
+        // date
+        var d = new Date();
+        localStorage.setItem("lastDateVisited", d.getTime());
+    } else if (localStorage.getItem("visited") == "true") {
+        var lastDateVisited = localStorage.getItem("lastDateVisited");
+        var currentDate = new Date().getTime();
+        if ((lastDateVisited + (30 * 24 * 60 * 60 * 1000)) <= currentDate) {
+            localStorage.setItem("visited", "false");
+        }
+    }
     // date and time stuff
     var date = new Date();
     var time_hours = date.getHours();
-    if(time_hours < 12) {
+    if (time_hours < 12) {
         time_p.innerText = "Good Morning";
-    } else if(time_hours >= 12 && time_hours <= 16) {
+    } else if (time_hours >= 12 && time_hours <= 16) {
         time_p.innerText = "Good Afternoon";
-    } else if(time_hours > 16) {
+    } else if (time_hours > 16) {
         time_p.innerText = "Good Evening";
     }
 } else {
@@ -55,3 +70,29 @@ rpsCarousel_div.addEventListener("click", () => goToURL("https://aaditya-baduni.
 teachSoundsCarousel_div.addEventListener("click", () => goToURL("https://aaditya-baduni.github.io/other/teach-sounds/"));
 faqCarousel_div.addEventListener("click", () => goToURL("https://aaditya-baduni.github.io/other/faq/"));
 aboutMeCarousel_div.addEventListener("click", () => goToURL("https://aaditya-baduni.github.io/other/about-me/"));
+
+// Welcome screen
+var welcome_div = document.getElementById("welcome");
+var continue_btn = document.getElementById("continue");
+var visited = localStorage.getItem("visited");
+
+if (visited == "true") {
+    welcome_div.style.height = "0%";
+    welcome_div.style.width = "0%";
+    welcome_div.style.display = "none";
+} else {
+    continue_btn.onclick = function () {
+        var opacity = 1;
+        var opa = setInterval(() => {
+            if (opacity > 0.0) {
+                opacity -= 0.1;
+                welcome_div.style.opacity = opacity.toString();
+            } else {
+                clearInterval(opa);
+                welcome_div.style.height = "0%";
+                welcome_div.style.width = "0%";
+                welcome_div.style.display = "none";
+            }
+        }, 20);
+    };
+}
